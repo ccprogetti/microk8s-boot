3302abb2d2dedf3ec8e65254047581e19cc52dab
Cavalieri Andrea <cons.CavalieriAndrea@p4cards.eu>
2020-11-03 01:15:11 +0100

    test refactory

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,27 @@
 package com.example.demo;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class DemoApplicationTests {
-  @Test
-  void contextLoads() {
-  }
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void homeResponse() {
+		String body = this.restTemplate.getForObject("/", String.class);
+		assertThat(body).isEqualTo("Spring is here!");
+	}
 }
