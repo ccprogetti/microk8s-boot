7ce99b8cf9f760a4d433ca0cd12ff89aea526cb1
andrea-cavalieri <andrea.cavalieri@addvalue.it>
2022-01-20 11:47:32 +0000

    refactory

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,9 @@ import java.util.UUID;
 
 @SpringBootApplication
 @RestController
-@Slf4j
 public class DemoApplication {
+
+  Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
@@ -21,7 +23,15 @@ public class DemoApplication {
   public String home() {
   	String dummy = "Spring is here!";
     String event = UUID.randomUUID().toString();
-    log.info(event);
+    
+    double x = 0.001;
+    for (int i = 0; i < 1000000; i++) {
+      x = java.lang.Math.sqrt(x);
+    }
+
+    logger.info(String.valueOf(x));
+    logger.info(event);
+    
     return dummy;
   }
 }
