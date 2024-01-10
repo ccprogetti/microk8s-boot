e251e9ac2afa529c07aa0b6abeb32d033690b975
andrea-cavalieri <97693200+andrea-cavalieri@users.noreply.github.com>
2024-01-10 11:57:06 +0100

    print var

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import java.util.UUID;
 
@@ -13,6 +15,9 @@ import java.util.UUID;
 @Slf4j
 public class DemoApplication {
 
+  @Autowired
+  private Environment environment;
+
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
@@ -20,7 +25,8 @@ public class DemoApplication {
 
   @GetMapping("/")
   public String home() {
-  	String dummy = "Spring is here again!";
+    String tmp = environment.getProperty("MICROK_8_S_BOOT_GIT_PORT_8080_TCP")
+  	String dummy = "Spring is here again!" + tmp;
     String event = UUID.randomUUID().toString();
     log.info(event);
     return dummy;
